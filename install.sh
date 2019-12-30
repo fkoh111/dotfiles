720d9ebf672686f7640bd6301df52f0fa81f5ff3
Frederik Kok Hansen <frederikkokhansen@pc01762.leo-internal.net>
2019-12-31 00:59:18 +0100

    Will no longer append forever to bash_profile

--- install.sh
+++ install.sh
@@ -9,13 +9,12 @@ echo " "
 PATH_ALIAS=$PWD/alias
 EXE_ALIAS=$PATH_ALIAS/.alias
 
-## COPY .ALIAS TO $HOME
+## Copy .alias to $HOME
 if [ -f $EXE_ALIAS ]; then
    cp -f $EXE_ALIAS $HOME
 else
    echo "Whoops, I thought there would be an alias here..."
 fi
 
-echo "# -------------" >> ~/.bash_profile
-echo "source ~/.alias" >> ~/.bash_profile
-echo "# -------------" >> ~/.bash_profile
+## Append a source cmd to .bash_profile if it isn't there all ready
+grep -qxF 'source ~/.alias' ~/.bash_profile || echo 'source ~/.alias' >> ~/.bash_profile
