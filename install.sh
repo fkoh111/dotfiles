75bb46cfad2dce837925db6a175c5822fc9076c6
fkoh111 <frederik_kok@icloud.com>
2020-01-04 17:50:08 +0100

    utilized security from cp, add .R*, etc. etc.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo " "
 echo "------------------------------------------"
@@ -6,15 +6,35 @@ echo "  About to setup dotfiles, aliases, etc.  "
 echo "------------------------------------------"
 echo " "
 
-PATH_ALIAS=$PWD/alias
-EXE_ALIAS=$PATH_ALIAS/.alias
+DOTFILES=$PWD/dots/
+BASHRC=$PWD/bashrc/.bashrc
 
-## Copy .alias to $HOME
-if [ -f $EXE_ALIAS ]; then
-   cp -f $EXE_ALIAS $HOME
-else
-   echo "Whoops, I thought there would be an alias here..."
-fi
+## Enable nullglob
+shopt -s dotglob
+
+echo " "
+echo "COPYING DOTFILES TO $HOME"
+
+for file in $DOTFILES.*[A-Za-z]; do
+  cp -vi "$file" $HOME
+done
+
+echo " "
+echo "COPYING .bashrc TO $HOME"
+
+for file in $BASHRC; do
+  cp -vi "$file" $HOME
+done
+
 
 ## Append a source cmd to .bash_profile if it isn't there all ready
-grep -qxF 'source ~/.alias' ~/.bash_profile || echo 'source ~/.alias' >> ~/.bash_profile
+grep -qxF 'source ~/.bashrc' ~/.bash_profile || echo 'source ~/.bashrc' >> ~/.bash_profile
+
+## Disable nullglob
+shopt -u nullglob
+
+echo " "
+echo "------------------------------------------"
+echo "  Splendid! You're all set :-D            "
+echo "------------------------------------------"
+echo " "
