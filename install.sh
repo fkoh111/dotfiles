0673271c55379cc4e5da91fcac8981b541752388
fkoh111 <frederik_kok@icloud.com>
2020-01-05 13:38:22 +0100

    Testing for bashrc and bash_profile

--- install.sh
+++ install.sh
@@ -6,27 +6,54 @@ echo "  About to setup dotfiles, aliases, etc.  "
 echo "------------------------------------------"
 echo " "
 
-DOTFILES=$PWD/dots/
-BASHRC=$PWD/bashrc/.bashrc
 
+DOTFILES=$PWD/dots/
+DOTFILE=$PWD/interface/.dotfile
 
 ## Source utils
 . $PWD/utils/utils.sh
 
+
 ## Enable nullglob
 shopt -s dotglob
 
-echo " "
-echo "COPYING DOTFILES TO $HOME"
 
-for file in $DOTFILES.*[A-Za-z]; do
-  cp -vi "$file" $HOME
-done
+## Test for .bashrc in $HOME
+if [ -f $HOME/.bashrc ]
+  then
+    echo "Seems like we have a .bashrc file"
+    TARGET=$HOME/.bashrc
+    echo "$TARGET this is from target bashrc"
+fi
+
+
+## Test for .bash_profile in $HOME
+if [ -f $HOME/.bash_profile ]
+  then
+    echo "Seems like we have a .bash_profile"
+    TARGET=$HOME/.bash_profile
+    echo "$TARGET this is from target bash profile"
+  else
+    echo "WHUT?! Why don't you have a bash_profile or bashrc?!"
+fi
+
+
+echo " "
+echo "COPYING .dotfile TO $HOME"
+
+cp -vi $PWD/interface/.dotfile $HOME
+
 
 echo " "
 echo "COPYING .bashrc TO $HOME"
 
-for file in $BASHRC; do
+cp -vi $PWD/bashrc/.bashrc $HOME
+
+
+echo " "
+echo "COPYING . FROM DOTFILES TO $HOME"
+
+for file in $DOTFILES.*[A-Za-z]; do
   cp -vi "$file" $HOME
 done
 
@@ -34,8 +61,10 @@ done
 ## Append a source cmd to .bash_profile if it isn't there all ready
 append 'source ~/.bashrc' ~/.bash_profile
 
+
 ## Disable nullglob
 shopt -u nullglob
+
 
 echo " "
 echo "------------------------------------------"
--- utils/utils.sh
+++ utils/utils.sh
@@ -3,9 +3,8 @@
 append() {
   ADD_EXPRESSION=$1
   TARGET=$2  
+  echo " "
   echo "Appending expression $ADD_EXPRESSION to $TARGET"
 
   grep -qxF "$ADD_EXPRESSION" "$TARGET" || echo "$ADD_EXPRESSION" >> "$TARGET"
 }
-
-append 'source ~/.bashrc' ~/.bash_profile
